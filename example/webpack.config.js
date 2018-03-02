b945d42df0ea1333a6268620a9b6acd4fe2c3f81
Raiondesu <a.raiondesu@gmail.com>
2018-03-02 20:00:50 +0300

    Moved index.html to src.

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, 'index.html')
+      template: path.resolve(__dirname, 'src/index.html')
     })
   ]
 }
