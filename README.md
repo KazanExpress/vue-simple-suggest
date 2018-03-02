d90b02e8f8b15f3ff71b13ad2762c73d5bb8859f
Raiondesu <a.raiondesu@gmail.com>
2018-03-02 18:24:05 +0300

    Workspace

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vue-simple-suggest
-Suggest component for Vue.js
+
+> Suggest component for Vue.js
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve example with hot reload at localhost:8080
+npm run example
+```
---
+++ example/webpack.config.js
@@ -0,0 +1,73 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const root = './example'
+
+module.exports = {
+  entry: root + '/src/main.js',
+  output: {
+    path: path.resolve(__dirname, root + '/dist'),
+    publicPath: root + '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  optimization: {
+    minimize: true
+  },
+  // plugins: [
+  //   new HtmlWebPackPlugin({
+  //     template: root + "src/index.html",
+  //     filename: root + "index.html"
+  //   })
+  // ]
+}
