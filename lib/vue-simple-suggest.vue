2aeac686bc3ad3c818188a220ee310d9849f8dd4
Thierry Blind <tbl0605@gmail.com>
2023-09-12 16:35:55 +0200

    Update vue-simple-suggest.vue

--- lib/vue-simple-suggest.vue
+++ lib/vue-simple-suggest.vue
@@ -30,7 +30,7 @@
         <li class="suggest-item" v-for="(suggestion, index) in suggestions"
           role="option"
           @mouseenter="hover(suggestion, $event.target)"
-          @mouseleave="hover(undefined)"
+          @mouseleave="hover(null)"
           @click="suggestionClick(suggestion, $event)"
           :aria-selected="(isHovered(suggestion) || isSelected(suggestion)) ? 'true' : 'false'"
           :id="getId(suggestion, index)"
