e24003dbfd1f11df1e4f0a20b1080a38cc9013f0
Thierry Blind <tbl0605@gmail.com>
2023-09-15 15:41:53 +0200

    Add fix #590

--- lib/vue-simple-suggest.vue
+++ lib/vue-simple-suggest.vue
@@ -687,7 +687,7 @@ export default {
         }
       } finally {
         if (this.maxSuggestions) {
-          result.splice(this.maxSuggestions)
+          result = result.slice(0, this.maxSuggestions)
         }
 
         this.isPlainSuggestion = nextIsPlainSuggestion
